9cbd48617ab799e560706ad0fb8603c5f7ef171b
Marce Frasson <marcefrasson@gmail.com>
2021-11-30 17:34:33 -0300

    comenting

--- source/reading_line.c
+++ source/reading_line.c
@@ -6,7 +6,7 @@
 /*   By: mfrasson <mfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 17:04:46 by mfrasson          #+#    #+#             */
-/*   Updated: 2021/11/30 17:13:29 by mfrasson         ###   ########.fr       */
+/*   Updated: 2021/11/30 17:34:14 by mfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,20 +35,24 @@ char    *create_prompt(char *chars)
 
 int main(void)
 {
-    HIST_ENTRY *history;
+    HIST_ENTRY *history;    // this type comes from history.h
     char *line_read = (char *)NULL;
     char *prompt;
 
+    // prompt will store the string passed to create_prompt
     prompt = create_prompt("️‍🔥 > ");
+    // line_read stores the input line
     line_read = readline(prompt);
 
+    // stores line in history
     if (line_read && *line_read)
         add_history(line_read);
 
-    printf("line_read: %s\n", line_read);
+    printf("\nline_read: %s\n", line_read);
 
+    // gets line history by index
     history = history_get(1);
 
-    printf("history: %s\n", history->line);
+    printf("\nhistory: %s\n\n", history->line);
     return (0);
 }
