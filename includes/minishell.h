801b7e8d1009de0b33275eb4ba820f8085107261
Italo aurelio <italoaurelior@gmail.com>
2021-12-04 12:52:17 -0300

    feat: add if on header

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,18 +6,22 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/30 17:04:35 by mfrasson          #+#    #+#             */
-/*   Updated: 2021/12/04 11:43:01 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/12/04 12:26:22 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <sys/wait.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <sys/types.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include "../libft/libft.h"
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <sys/wait.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <sys/types.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "../libft/libft.h"
 
-#define BUFFERSIZE 1024
+# define BUFFERSIZE 1024
+
+# endif
