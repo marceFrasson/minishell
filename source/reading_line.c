def9ee97e5531f086c2c7ec7a3d2e75e37668464
Marce Frasson <marcefrasson@gmail.com>
2021-12-04 17:51:24 -0300

    charjoin

--- source/reading_line.c
+++ source/reading_line.c
@@ -12,6 +12,33 @@
 
 #include "../includes/minishell.h"
 
+char	*ft_charjoin(char const *string1, char const c)
+{
+	char	*string;
+	int		i;
+	int		end_string1;
+
+	if (!string1 || !c)
+		return (NULL);
+	string = (char *)malloc(((ft_strlen(string1)) + 2));
+	if (string == NULL)
+		return (NULL);
+	i = 0;
+	while (string1[i] != '\0')
+	{
+		string[i] = string1[i];
+		i++;
+	}
+	end_string1 = i;
+	i = 0;
+	while (c != '\0')
+	{
+		string[end_string1 + i] = c;
+		i++;
+	}
+	string[end_string1 + i] = '\0';
+	return (string);
+}
 
 char	*ft_substr_shrink(char const *string, unsigned int start, size_t len)
 {
