829189e4061ead4e95c87e5e406b92c5864b038e
Italo aurelio <italoaurelior@gmail.com>
2021-12-08 06:45:59 -0300

    wrote logic

---
+++ source/builtins.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/04 13:07:34 by itaureli          #+#    #+#             */
+/*   Updated: 2021/12/08 06:45:49 by itaureli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+◦ echo with option -n
+◦ cd with only a relative or absolute path
+◦ pwd with no options
+◦ export with no options
+◦ unset with no options
+◦ env with no options or arguments
+◦ exit with no options
+
+Get array of commands for each command execute a builtin command
+*/
+
+static int check_builtins(char **command)
+{
+
+}
--- source/exec.c
+++ source/exec.c
@@ -6,7 +6,7 @@
 /*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/08 06:19:41 by itaureli          #+#    #+#             */
-/*   Updated: 2021/12/08 06:42:59 by itaureli         ###   ########.fr       */
+/*   Updated: 2021/12/08 06:45:24 by itaureli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,9 @@
 
 int	check_pipes(char **tokens)
 {
+	/*
+	Check if there's pipe command
+	*/
 	int i;
 
 	i = 0;
@@ -32,6 +35,12 @@ int	exec_commands(char *input_line)
 	fake_tokens[0] = "echo";
 	fake_tokens[1] = "hello world";
 
+	/*
+		if is pipe
+		go to pipex_exec
+		else
+		go to exec
+	*/
 	check_pipes(fake_tokens);
 	return(0);
 }
