3dc38a114c3e97631d451bd0958f691ae3d3678b
Italo aurelio <italoaurelior@gmail.com>
2021-12-20 22:33:56 -0300

    prepare files

---
+++ source/pipes.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipes.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/04 13:01:51 by itaureli          #+#    #+#             */
+/*   Updated: 2021/12/04 19:26:50 by itaureli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Pipes | The output of each command in the pipeline is connected via a pipe to the
+input of the next command.
+*/
+
---
+++ source/redirections.c
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirections.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itaureli <itaureli@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/04 13:06:25 by itaureli          #+#    #+#             */
+/*   Updated: 2021/12/04 13:06:28 by itaureli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+Redirections:
+◦ < should redirect input.
+◦ > should redirect output.
+◦ “<<” read input from the current source until a line containing only the delimiter is seen. it doesn’t need to update history!
+◦ “>>” should redirect output with append mode
+*/
